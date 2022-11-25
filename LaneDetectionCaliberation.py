7a5d6b1e2ef1294dc5aeb7b76749349c0d8a2bfc
uttra122 <116025694+uttra122@users.noreply.github.com>
2022-11-26 00:27:08 +0530

    committing a missing class

--- LaneDetectionCaliberation.py
+++ LaneDetectionCaliberation.py
@@ -312,6 +312,43 @@ def threshold_rel(img, lo, hi):
 
 def threshold_abs(img, lo, hi):
     return np.uint8((img >= lo) & (img <= hi)) * 255
+
+# Greying of image
+class Threshold:
+    """ This class is for extracting relevant pixels in an image.
+    """
+
+    def __init__(self):
+        """ Init Thresholding."""
+        pass
+
+    def forward(self, img):
+        """ Take an image and extract all relavant pixels.
+
+        Parameters:
+            img (np.array): Input image
+
+        Returns:
+            binary (np.array): A binary image represent all positions of relavant pixels.
+        """
+        hls = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
+        hsv = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+        h_channel = hls[:, :, 0]
+        l_channel = hls[:, :, 1]
+        s_channel = hls[:, :, 2]
+        v_channel = hsv[:, :, 2]
+
+        right_lane = threshold_rel(l_channel, 0.8, 1.0)
+        right_lane[:, :750] = 0
+
+        left_lane = threshold_abs(h_channel, 20, 30)
+        left_lane &= threshold_rel(v_channel, 0.7, 1.0)
+        left_lane[:, 550:] = 0
+
+        img2 = left_lane | right_lane
+        plt.imshow(img2)
+        plt.show()
+        return img2
     
 # Region of interest
 class PerspectiveTransformation:
